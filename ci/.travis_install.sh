b7d393bc2ac28331fa0e8147cc8895f6931450b7
Tilak Raj Singh <tilak72@gmail.com>
2018-11-02 02:43:45 -0400

    fixing mkdocs installation

--- ci/.travis_install.sh
+++ ci/.travis_install.sh
@@ -7,7 +7,10 @@ echo "sudo cmake version:"
 sudo cmake --version
 
 echo "installing mkdocs"
-pip install mkdocs
+sudo pip install mkdocs
+
+echo "mkdocsa version"
+mkdocs --version
 
 echo "installing sample..."
 mkdir build;
--- ci/.travis_test.sh
+++ ci/.travis_test.sh
@@ -2,13 +2,14 @@ echo "running sample.."
 ./build/sample
 
 if [[ $TRAVIS_BRANCH == 'master' ]]
-echo "building doxygen docs"
-cd docs
-doxygen Doxyfile
+    echo "building doxygen docs"
+    cd docs
+    doxygen Doxyfile
 
-echo "docs build successfully"
+    echo "docs build successfully"
 
-cd ..
+    cd ..
 
-echo "Building mkdocs"
-mkdocs build --verbose --clean --strict
+    echo "Building mkdocs"
+    mkdocs build --verbose --clean --strict
+fi
